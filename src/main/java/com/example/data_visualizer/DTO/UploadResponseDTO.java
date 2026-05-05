1539c9c71e48bee51d09305436246dfd7488f58c
devesh-chaudhary-2004 <chaudharydebu008@gmail.com>
2026-05-05 14:36:05 +0530

    code added

--- src/main/java/com/example/data_visualizer/DTO/UploadResponseDTO.java
+++ src/main/java/com/example/data_visualizer/DTO/UploadResponseDTO.java
@@ -1,4 +1,17 @@
 package com.example.data_visualizer.DTO;
+package com.example.dashboard.dto;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class UploadResponseDTO {
+
+    private List<ColumnInfoDTO> columns;
+
+    private List<ChartConfigDTO> charts;
+
+    private List<Map<String, Object>> data;
 }
