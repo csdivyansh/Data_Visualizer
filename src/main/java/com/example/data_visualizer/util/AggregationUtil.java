ded93fcfbb252a57589880aa36b21022496e6679
csdivyansh <idivyanshv@gmail.com>
2026-05-05 14:36:44 +0530

    Added FilterUtil

--- src/main/java/com/example/data_visualizer/util/AggregationUtil.java
+++ src/main/java/com/example/data_visualizer/util/AggregationUtil.java
@@ -1,4 +1,29 @@
-package com.example.data_visualizer.util;
+package com.example.dashboard.util;
+
+import java.util.List;
+import java.util.Map;
 
 public class AggregationUtil {
+
+    public static double sum(
+            List<Map<String, Object>> data,
+            String column
+    ) {
+
+        double sum = 0;
+
+        for (Map<String, Object> row : data) {
+
+            Object val = row.get(column);
+
+            if (val != null) {
+
+                sum += Double.parseDouble(
+                        val.toString()
+                );
+            }
+        }
+
+        return sum;
+    }
 }
