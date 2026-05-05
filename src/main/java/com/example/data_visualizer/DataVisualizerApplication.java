24dbfc5ff2576512683d0855aab4d187d00a2d36
csdivyansh <idivyanshv@gmail.com>
2026-05-05 13:29:59 +0530

    Initial

---
+++ src/main/java/com/example/data_visualizer/DataVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataVisualizerApplication.class, args);
+    }
+
+}
