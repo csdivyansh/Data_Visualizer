8435097d47ba9335be87263a51935e87df9cdbaa
Gun-Saxena <gunsaxena2004@gmail.com>
2026-05-05 14:53:45 +0530

    Added FileUploadController

--- src/main/java/com/example/data_visualizer/service/CsvParserService.java
+++ src/main/java/com/example/data_visualizer/service/CsvParserService.java
@@ -1,4 +1,35 @@
 package com.example.data_visualizer.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CsvParserService {
+
+    public List<Map<String, Object>> parseCSV(MultipartFile file)
+            throws Exception {
+
+        List<Map<String, Object>> data = new ArrayList<>();
+
+        CSVReader reader = new CSVReader(
+                new InputStreamReader(file.getInputStream())
+        );
+
+        List<String[]> rows = reader.readAll();
+
+        String[] headers = rows.get(0);
+
+        for (int i = 1; i < rows.size(); i++) {
+
+            Map<String, Object> rowMap = new HashMap<>();
+
+            for (int j = 0; j < headers.length; j++) {
+
+                rowMap.put(headers[j], rows.get(i)[j]);
+            }
+
+            data.add(rowMap);
+        }
+
+        return data;
+    }
 }
